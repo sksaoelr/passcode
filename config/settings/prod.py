ee0410b8dc22d4b75312b05705f68cfe8422f83d
dawoon <dawoon_jung@mycelebs.com>
2022-12-30 10:01:23 +0900

    add: domain update

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.200.35.245']
+ALLOWED_HOSTS = ['43.200.35.245', 'passcode.or.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
